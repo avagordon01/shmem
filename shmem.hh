0b293b7183f58ef18dcdce1c2d955f97abe9271e
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-03-02 00:14:52 +0000

    trying to get allocator working

--- shmem.hh
+++ shmem.hh
@@ -8,18 +8,25 @@
 #include <string>
 
 class shmem {
+    int fd;
     std::string path;
-    size_t size;
     void* address;
 
 public:
+    size_t size;
     shmem(std::string path_, size_t size_, void* address_ = nullptr):
-        path(path_), size(size_) {
-        int fd = shm_open(path.c_str(), O_CREAT | O_EXCL | O_RDWR, S_IRWXU);
+        path(path_) {
+        fd = shm_open(path.c_str(), O_CREAT | O_EXCL | O_RDWR, S_IRWXU);
         assert(fd && "failed to shm_open");
-        int ret = ftruncate(fd, size);
-        assert(!ret && "failed to ftruncate");
+        resize(size_);
         address = mmap(address_, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        fprintf(stderr, "shmem fd %i address %p\n", fd, address);
+    }
+    void resize(size_t size_) {
+        int ret = ftruncate(fd, size_);
+        fprintf(stderr, "ftruncate fd %i size %zu\n", fd, size_);
+        assert(!ret && "failed to ftruncate");
+        size = size_;
     }
     operator void*() {
         return address;
@@ -52,3 +59,92 @@ public:
         assert(!ret);
     }
 };
+
+template <class T>
+class shmem_allocator {
+    shmem s = {"alloc", 0, (void*)0x5f0000};
+
+public:
+    using value_type    = T;
+
+//     using pointer       = value_type*;
+//     using const_pointer = typename std::pointer_traits<pointer>::template
+//                                                     rebind<value_type const>;
+//     using void_pointer       = typename std::pointer_traits<pointer>::template
+//                                                           rebind<void>;
+//     using const_void_pointer = typename std::pointer_traits<pointer>::template
+//                                                           rebind<const void>;
+
+//     using difference_type = typename std::pointer_traits<pointer>::difference_type;
+//     using size_type       = std::make_unsigned_t<difference_type>;
+
+//     template <class U> struct rebind {typedef shmem_allocator<U> other;};
+
+    shmem_allocator() noexcept {
+        fprintf(stderr, "shmem_allocator ctor\n");
+    }  // not required, unless used
+    template <class U>
+    shmem_allocator(shmem_allocator<U> const&) noexcept {
+        fprintf(stderr, "shmem_allocator ctor\n");
+    }
+
+    value_type* // Use pointer if pointer is not a value_type*
+    allocate(std::size_t n) {
+        fprintf(stderr, "shmem_allocator allocate\n");
+        return static_cast<value_type*>(::operator new (n*sizeof(value_type)));
+        size_t offset = s.size;
+        s.resize(s.size + sizeof(value_type) * n);
+        return static_cast<value_type*>(((void*)s) + offset);
+    }
+
+    void deallocate(value_type* p, std::size_t) noexcept {// Use pointer if pointer is not a value_type*
+        //::operator delete(p);
+    }
+
+//     value_type*
+//     allocate(std::size_t n, const_void_pointer)
+//     {
+//         return allocate(n);
+//     }
+
+//     template <class U, class ...Args>
+//     void
+//     construct(U* p, Args&& ...args)
+//     {
+//         ::new(p) U(std::forward<Args>(args)...);
+//     }
+
+//     template <class U>
+//     void
+//     destroy(U* p) noexcept
+//     {
+//         p->~U();
+//     }
+
+//     std::size_t
+//     max_size() const noexcept
+//     {
+//         return std::numeric_limits<size_type>::max();
+//     }
+
+//     shmem_allocator
+//     select_on_container_copy_construction() const
+//     {
+//         return *this;
+//     }
+
+//     using propagate_on_container_copy_assignment = std::false_type;
+//     using propagate_on_container_move_assignment = std::false_type;
+//     using propagate_on_container_swap            = std::false_type;
+//     using is_always_equal                        = std::is_empty<shmem_allocator>;
+};
+
+template <class T, class U>
+bool operator==(shmem_allocator<T> const&, shmem_allocator<U> const&) noexcept {
+    return false;
+}
+
+template <class T, class U>
+bool operator!=(shmem_allocator<T> const& x, shmem_allocator<U> const& y) noexcept {
+    return !(x == y);
+}
--- shmem_test.cc
+++ shmem_test.cc
@@ -1,12 +1,20 @@
 #include "shmem.hh"
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "usage: " << argv[0] << " (shmem|shmem_view)" << std::endl;
         return 1;
     }
+
+    shmem_allocator<char> a;
+    //std::vector<char, shmem_allocator<char>> v;
+    //v.push_back('c');
+    //std::cout << v.back() << std::endl;
+
+    return 0;
 
     char str[] = "test";
     void* shared_address = (void*)0x3f0000;
@@ -15,6 +23,7 @@ int main(int argc, char* argv[]) {
         printf("writing into address %p\n", (void*)s);
 
         memcpy(s, str, sizeof(str));
+        s.resize(4);
 
         sleep(2);
     } else if (strcmp(argv[1], "shmem_view") == 0) {
@@ -28,6 +37,7 @@ int main(int argc, char* argv[]) {
         } else {
             std::cout << "bad" << std::endl;
         }
+        std::cout << "contents: \"" << (char*)(void*)s << "\"" << std::endl;
     } else {
         std::cerr << "usage: " << argv[0] << "(a|b)" << std::endl;
         return 1;
