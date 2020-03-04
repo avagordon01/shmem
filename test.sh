21a9d173065d17c81871200733385fd4bbd491a8
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-03-04 21:04:52 +0000

    better cleanup in test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 set -eu
-trap 'kill $(jobs -p) 2>/dev/null' EXIT
+
+function cleanup {
+    set +e
+    {
+        kill $(jobs -p)
+        rm -rf /dev/shm/*
+    } 2>/dev/null
+}
+trap cleanup EXIT
 
 ninja -C build
 
 ./build/shmem_test shmem &
 ./build/shmem_test shmem_view &
 wait
-
-rm -rf /dev/shm/aether_ro
